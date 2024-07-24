8318cc4c4112b2c6f6e3d97425018cf0b4b8bebe
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-24 16:54:25 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -51,19 +51,23 @@ function App() {
             <img src="/assets/images/image-retro-pcs.jpg" alt="image-retro-pcs" />
           </div>
           <div className='final-text'>
-            <h2 style={{color: 'var(--soft-red)', fontSize: '2em'}}>01</h2>
-            <p className='final-container-sub-header' style={{fontSize:'1.1em', fontWeight: '700'}}>Reviving Retro PCs</p>
-            <p>What happens when old PCs are given modern upgrades?</p>
-          </div>
+            <div className='inner-final-text'>
+            <h2 style={{color: 'var(--soft-red)', fontSize: '2em',marginBottom: '-0.2em'}}>01</h2>
+            <p className='final-container-sub-header' style={{fontSize:'1.1em', fontWeight: '700',marginBottom: '-0.2em'}}>Reviving Retro PCs</p>
+            <p className='last-final-text'>What happens when old PCs are given modern upgrades?</p>      
+            </div>
+            </div>
         </div>
         <div className='inner-final-container'>
           <div className='image'>
             <img src="/assets/images/image-top-laptops.jpg" alt="image-top-laptops" />
           </div>
           <div className='final-text'>
-            <h2 style={{color: 'var(--soft-red)', fontSize: '2em'}}>02</h2>
-            <p  className='final-container-sub-header' style={{fontSize:'1.1em', fontWeight: '700'}}>Top 10 Laptops of 2022</p>
-            <p>Our best picks for various needs and budgets.</p>
+            <div className='inner-final-text'>
+            <h2 style={{color: 'var(--soft-red)', fontSize: '2em', marginBottom: '-0.3em'}}>02</h2>
+            <p  className='final-container-sub-header' style={{fontSize:'1.1em', fontWeight: '700',marginBottom: '-0.2em'}}>Top 10 Laptops of 2022</p>
+            <p className='last-final-text'>Our best picks for various needs and budgets.</p>
+            </div>
           </div>
         </div>
         <div className='inner-final-container'>
@@ -71,10 +75,10 @@ function App() {
             <img src="/assets/images/image-gaming-growth.jpg" alt="image-gaming-growth" />
           </div>
           <div className='final-text'>
-            <div>
-            <h2 style={{color: 'var(--soft-red)', fontSize: '2em'}}>03</h2>
-            <p   className='final-container-sub-header'style={{fontSize:'1.1em', fontWeight: '700'}}>The Growth of Gaming</p>
-            <p>How the pandemic has sparked fresh opportunities</p>
+            <div className='inner-final-text'>
+            <h2 style={{color: 'var(--soft-red)', fontSize: '2em',marginBottom: '-0.2em'}}>03</h2>
+            <p   className='final-container-sub-header' style={{fontSize:'1.1em', fontWeight: '700',marginBottom: '-0.2em'}}>The Growth of Gaming</p>
+            <p className='last-final-text'>How the pandemic has sparked fresh opportunities</p>
           </div>
           </div>
         </div>
@@ -83,4 +87,4 @@ function App() {
   )
 }
 
-export default App 
+export default App
