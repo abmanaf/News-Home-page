1a2a2c400f607d0f894eb24826dd547109934826
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-26 07:32:38 +0000

    changes made

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-
+import React, { useState } from 'react';
 
 function NavBar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div>
-        <div className='header'>
-            <div className='site-logo'>
-                <img src="/assets/images/logo.svg" alt="logo" />
-            </div>
-            <div className='header-items'>
-                <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">New</a></li>
-                <li><a href="#">Popular</a></li>
-                <li><a href="#">Trending</a></li>
-                <li><a href="#">Category</a></li>
-                </ul>
-            </div>
-            <div className='menu-icon'>
-                <img src="/assets/images/icon-menu.svg" alt="icon-menu" />
-            </div>
+      <div className='header'>
+        <div className='site-logo'>
+          <img src="/assets/images/logo.svg" alt="logo" />
         </div>
-     </div>
-  )
+        <div className='header-items'>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">New</a></li>
+            <li><a href="#">Popular</a></li>
+            <li><a href="#">Trending</a></li>
+            <li><a href="#">Category</a></li>
+          </ul>
+        </div>
+        <div className='menu-icon' onClick={toggleSidebar}>
+          <img src="/assets/images/icon-menu.svg" alt="icon-menu" />
+        </div>
+      </div>
+      <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+        <button className='close-button' onClick={toggleSidebar}><img src="/assets/images/icon-menu-close.svg" alt="" /></button>
+        <ul>
+          <li><a sty href="#">Home</a></li>
+          <li><a href="#">New</a></li>
+          <li><a href="#">Popular</a></li>
+          <li><a href="#">Trending</a></li>
+          <li><a href="#">Category</a></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
