df9e937882db771f444297b3dd2747dbe94a280a
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-28 02:11:23 +0000

    minor changes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-//import './App.css'
 
 function NavBar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
-    
     //setIsSidebarOpen(true)
   };
 
