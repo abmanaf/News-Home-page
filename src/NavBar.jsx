1d3957c62a5512fa0677272526fd169c9cb334b2
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-28 02:11:47 +0000

    I removed commented codes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 //import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 
 function NavBar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
